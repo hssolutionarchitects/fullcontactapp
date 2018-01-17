a45e4341b736d1f6f9086ce5c695f14e73c14164
Turner Vink <turner.vink@hootsuite.com>
2018-01-16 16:04:38 -0800

    Tidied up TODOs

--- static/js/plugin.js
+++ static/js/plugin.js
@@ -6,32 +6,18 @@ $(document).ready(function() {
     sendToAppHandler(message);
   });
 
-  /*
-    TODO: Currently two places where we show the popup in order to ensure
-    that Twitter data has been retrieved. Is there a better way?
-  */
   function sendToAppHandler(message) {
     if (message.post.network == 'TWITTER') {
-
+      // Get username from Twitter then show popup
       $.get('/api/tweet/' + message.post.id, function(data) {
-        console.log(data);
-        // TODO: screen_name or name?
         var username = "username=" + encodeURIComponent(data[0].user.screen_name);
-        console.log("Username: " + username);
-
         var handler = 'https://' + window.location.hostname + '/fullcontact_plugin_handler.html?' + username;
-        console.log(handler);
-
         hsp.showCustomPopup(handler, 'Full Contact Information');
       });
 
     } else {
       var username = "username=" + encodeURIComponent(message.post.user.username);
-      console.log("Username: " + username);
-
       var handler = 'https://' + window.location.hostname + '/fullcontact_plugin_handler.html?' + username;
-      console.log(handler);
-
       hsp.showCustomPopup(handler, 'Full Contact Information');
     }
   }
